6d55afe8fcea88e364eede65e1c5251af2e5ef5b
geges1101 <62469977+geges1101@users.noreply.github.com>
2022-11-01 17:42:15 +0300

    Add files via upload

---
+++ valid_palindrome.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.length() - 1;
+        while(l < r) {
+            if (!isalnum(s[l])) l++;
+            else if (!isalnum(s[r])) r--;
+            else {
+                if (tolower(s[l++]) != tolower(s[r--])) return false;
+            }
+        }
+        return true;
+    }
+};
