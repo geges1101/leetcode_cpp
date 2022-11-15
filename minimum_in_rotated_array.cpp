579776d644aaa1656d24c954263f2bbbbd1e7df7
geges1101 <62469977+geges1101@users.noreply.github.com>
2022-11-15 21:25:01 +0300

    Add files via upload

--- minimum_in_rotated_array.cpp
+++ minimum_in_rotated_array.cpp
@@ -8,12 +8,10 @@ public:
         while(left<=right){
             mid = (left+right)/2;
 
-            if(nums[mid] >= nums[right]){
+            if(nums[mid] >= nums[right])
                 left = mid+1;
-            }
-            else{
-                right = mid;
-            }
+
+            else right = mid;
         }
         return nums[mid];
 
