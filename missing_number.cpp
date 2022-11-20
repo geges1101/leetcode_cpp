fe0f26f99d1f1e7a96976d1d913495126156dd1a
geges1101 <62469977+geges1101@users.noreply.github.com>
2022-11-20 23:05:30 +0300

    Add files via upload

---
+++ missing_number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int count = 0;
+        for(int i = 0; i != n; i++){
+            if(count != nums[i]) return nums[i] -1;
+            count++;
+        }
+        return n;
+    }
+};
